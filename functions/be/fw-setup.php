650db4f152261557410e55f52dea021663fd38a7
Victor Tihai <victor@tihai.md>
2011-08-09 15:21:19 +0300

    009

--- functions/be/fw-setup.php
+++ functions/be/fw-setup.php
@@ -5,42 +5,6 @@
  * @subpackage BlogoLife
  * @since BlogoLife 1.0
 */
-
-function getCategories($parent) {
-	global $wpdb, $table_prefix;
-	$tb1 = "$table_prefix"."terms";
-	$tb2 = "$table_prefix"."term_taxonomy";
-	if ($parent == '0')	{
-		$qqq = "AND $tb2".".parent = 0";
-	} else	{
-		$qqq = "";
-	}
-	
-	$q = "SELECT $tb1.term_id,$tb1.name,$tb1.slug FROM $tb1,$tb2 WHERE $tb1.term_id = $tb2.term_id AND $tb2.taxonomy = 'category' $qqq ORDER BY $tb1.name ASC";
-	$q = $wpdb->get_results($q);
-	
-	foreach ($q as $cat) {
-		$categories[$cat->term_id] = $cat->name;
-	} 
-	return($categories);
-}
-		$categories = getCategories(0);
-		$categoriesParents = getCategories(0);
-		
-	if (count($categories) > 0) {
-	foreach ( $categories as $key => $value ) {
-			$catids[] = $key;
-			$catnames[] = $value;
-	}
-	}
-	if (count($categoriesParents) > 0){
-	foreach ( $categoriesParents as $key => $value ) {
-
-		$catidsp[] = $key;
-			$catnamesp[] = $value;
-		}
-}
-
 function wplook_bar_menu() {
 	global $wp_admin_bar;
 	if ( !is_super_admin() || !is_admin_bar_showing() )
