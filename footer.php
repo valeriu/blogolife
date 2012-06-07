e30319f6c9084b6e840ec77f621d5be8116dc7de
Victor Tihai <victor@tihai.md>
2012-06-07 10:57:37 +0300

    WPlook name ant title with caps

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 </div>
 		</div>
 		<!-- It is completely optional, but if you like the Theme I would appreciate it if you keep the credit link at the bottom. -->
-			<footer id="copy"><p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('wplook', 'wplook'); ?>" target="_blank">wplook</a></p></footer>
+			<footer id="copy"><p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('WPlook', 'wplook'); ?>" target="_blank">WPlook</a></p></footer>
 	<?php wp_footer(); ?>
 	</body>
 </html>
