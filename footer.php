75aa6daa216d25c961f33fabc4b899cf7253eea7
Victor Tihai <victor@tihai.md>
2012-08-10 13:31:12 +0300

    Issue #3

--- footer.php
+++ footer.php
@@ -9,8 +9,34 @@
 ?>
 </div>
 		</div>
-		<!-- It is completely optional, but if you like the Theme I would appreciate it if you keep the credit link at the bottom. -->
-			<footer id="copy"><p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('WPlook', 'wplook'); ?>" target="_blank">WPlook</a></p></footer>
+		<?php 
+		if (is_active_sidebar( 'col1-widgets' ) || is_active_sidebar( 'col2-widgets' ) || is_active_sidebar( 'col3-widgets' )	) { ?>
+			<div id="footer-widget-area">
+				<?php if ( is_active_sidebar( 'col1-widgets' ) ) : ?>
+					<!-- Widget area 1 -->
+					<div class="w1">
+						<?php dynamic_sidebar( 'col1-widgets' ); ?>
+					</div>
+				<?php endif; ?>
+				
+				<?php if ( is_active_sidebar( 'col2-widgets' ) ) : ?>
+					<!-- Widget area 2 -->
+					<div class="w2">
+						<?php dynamic_sidebar( 'col2-widgets' ); ?>
+					</div>
+				<?php endif; ?>
+				
+				<?php if ( is_active_sidebar( 'col3-widgets' ) ) : ?>
+					<!-- Widget area 3 -->
+					<div class="w3">
+						<?php dynamic_sidebar( 'col3-widgets' ); ?>
+					</div>
+				<?php endif; ?>
+				<div class="clear"></div>
+			</div>
+		<?php }	?>
+	<!-- It is completely optional, but if you like the Theme I would appreciate it if you keep the credit link at the bottom. -->
+	<footer id="copy"><p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('WPlook', 'wplook'); ?>" target="_blank">WPlook</a></p></footer>
 	<?php wp_footer(); ?>
 	</body>
 </html>
--- functions/fe/widgets-init.php
+++ functions/fe/widgets-init.php
@@ -17,7 +17,36 @@ function wplook_widgets_init() {
 		'before_title' => '<div class="widget-title"><h3>',
 		'after_title' => '</h3><div class="right-corner"></div></div>'
 	) );
-	
+	// Area 2, located in the footer.
+	register_sidebar( array(
+		'name' => __( 'First Footer Widget Area', 'wplook' ),
+		'id' => 'col1-widgets',
+		'description' => __( 'The first footer widget area', 'wplook' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<div class="widget-title"><h3>',
+		'after_title' => '</h3></div>'
+	) );
+	// Area 3, located in the footer.
+	register_sidebar( array(
+		'name' => __( 'Scond Footer Widget Area', 'wplook' ),
+		'id' => 'col2-widgets',
+		'description' => __( 'The second footer widget area', 'wplook' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<div class="widget-title"><h3>',
+		'after_title' => '</h3></div>'
+	) );
+	// Area 3, located in the footer.
+	register_sidebar( array(
+		'name' => __( 'Third Footer Widget Area', 'wplook' ),
+		'id' => 'col3-widgets',
+		'description' => __( 'The Third footer widget area', 'wplook' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<div class="widget-title"><h3>',
+		'after_title' => '</h3></div>'
+	) );	
 }
 /** Register sidebars */
 add_action( 'widgets_init', 'wplook_widgets_init' );
