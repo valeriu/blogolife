563e582d856692c3192d9c33e13de92450f52ac5
Victor Tihai <victor@tihai.md>
2012-08-30 11:57:12 +0300

    Issue #26

--- footer.php
+++ footer.php
@@ -36,7 +36,10 @@
 			</div>
 		<?php }	?>
 	<!-- It is completely optional, but if you like the Theme I would appreciate it if you keep the credit link at the bottom. -->
-	<footer id="copy"><p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('WPlook', 'wplook'); ?>" target="_blank">WPlook</a></p></footer>
+	<footer id="copy">
+		<p><?php _e('Proudly powered by', 'wplook'); ?> <a href="http://wordpress.org" target="_blank"><?php _e('WordPress', 'wplook'); ?></a>. <?php _e('Design by', 'wplook'); ?>  <a href="http://wplook.com/blogolifewpo" title="<?php _e('WPlook', 'wplook'); ?>" target="_blank">WPlook</a></p>
+		<span id="top"><a href="#"  title="<?php _e('Top', 'wplook'); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/top.png" width="7" height="16" /></a></span>
+	</footer>
 	<?php wp_footer(); ?>
 	</body>
 </html>
